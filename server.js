73cea04763a25279a6f0f9a2f8bc4848135cb154
Markus Voigt <markus.voigt90@gmail.com>
2023-11-06 09:09:28 +0100

    short cache

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ export default {
         i18n: {language: 'EN', country: 'US'},
         publicStorefrontToken: env.PUBLIC_STOREFRONT_API_TOKEN,
         privateStorefrontToken: env.PRIVATE_STOREFRONT_API_TOKEN,
-        storeDomain: env.PUBLIC_STORE_DOMAIN,
+        storeDomain: 'markusvoigt.myshopify.com',
         storefrontId: env.PUBLIC_STOREFRONT_ID,
         storefrontHeaders: getStorefrontHeaders(request),
       });
